02fa01a98ae7fed3a55970cbc8261d79377ece67
binhtdnd <binhtdnd@gmail.com>
2025-02-17 12:44:28 +0700

    test connection

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
 require("dotenv").config();
+import connection from "./config/connectDB";
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -13,6 +15,9 @@ configViewEngine(app);
 //config body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connection
+connection()
 
 //route
 initWebRoutes(app);
